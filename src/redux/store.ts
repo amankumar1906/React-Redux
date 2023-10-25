cdd45e06ce66e62ec18453951452e15302082f29
Aman Kumar <amankumar7506@gmail.com>
2023-10-25 14:03:33 -0400

    add Recorder action

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,10 @@
 import { combineReducers, createStore } from 'redux';
 import userEventReducer from './user-events';
+import recorderReducer from './recorder';
 
 const rootReducer = combineReducers({
   userEvents: userEventReducer,
+  recorder: recorderReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
